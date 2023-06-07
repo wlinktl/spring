2d21701f2261121b3d197e7acaddd106b348818a
wlinktl <wlinkcl@outlook.com>
2023-06-07 13:30:40 -0400

    add a new file

---
+++ spring_boot_3/demo/src/main/java/com/progress/demo/Util.java
@@ -0,0 +1,20 @@
+package com.progress.demo;
+
+public class Util {
+
+    //add a method to remove duplicates from an array of integers
+    public static int[] removeDuplicates(int[] arr) {
+        int[] result = new int[arr.length];
+        int j = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            //if the current element is not equal to the next element, add it to the result array
+            if (arr[i] != arr[i + 1]) {
+                result[j++] = arr[i];
+            }
+        }
+        //add the last element
+        result[j++] = arr[arr.length - 1];
+        return result;
+    }
+    
+}
